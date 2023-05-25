d0eafac31461236ec36bc488571443283c6f5bf9
suopuwu <Josephlu8@gmail.com>
2023-05-25 15:25:28 -0600

    A fresh start

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using suopCommerce.Models;
 
 namespace SuopCommerce.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private StoreContext db { get; set; } = new();
+        public int itemCount = 0;
 
         public void OnGet()
         {
-
+            itemCount = db.Products.Count();
         }
     }
 }
